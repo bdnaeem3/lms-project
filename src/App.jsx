90d40d8465fe37235371978b8b3193c032ec19e3
Naeem Ahmed <bdnaeem3@gmail.com>
2025-02-12 15:15:26 +0600

    Sessions Managment page split from Main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import Sidebar from './components/Sidebar'
-import Topbar from './components/Topbar'
-import Header from './components/Header'
-import Heading from './components/Heading'
+import SessionsManagement from './pages/SessionsManagement'
 
 function App() {
   return (
-    <main className='flex'>
-      <Sidebar />
-      <div className='w-full'>
-        <Topbar />
-        <div className='p-[25px]'>
-          <Header />
-          <Heading />
-        </div>
-      </div>
-    </main>
+    <SessionsManagement />
   )
 }
 
---
+++ src/pages/SessionsManagement.jsx
@@ -0,0 +1,21 @@
+import Sidebar from '../components/Sidebar'
+import Topbar from '../components/Topbar'
+import Header from '../components/Header'
+import Heading from '../components/Heading'
+
+function App() {
+  return (
+    <main className='flex'>
+      <Sidebar />
+      <div className='w-full'>
+        <Topbar />
+        <div className='p-[25px]'>
+          <Header />
+          <Heading />
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
